7eb83e765c4c171db96b3c5c2d7ef9d1fe55d163
Nerly Ton <nerlyton@gmail.com>
2020-07-31 20:00:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,7 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.select do |cheese|
-   cheese.include?("cheddar")
+  cheese_types.find{|cheese| cheese.include?("cheddar")}
   end
   cheese_types.join(" ")
 end
