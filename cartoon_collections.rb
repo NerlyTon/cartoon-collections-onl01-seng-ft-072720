e65e1a04a6eeb53af9bfbea2ec5d020efbdf6b6b
Nerly Ton <nerlyton@gmail.com>
2020-07-31 20:02:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find{|cheese| cheese.include?()}
+  cheese_types.find{|cheese| cheese.include?(cheddar)}
   end
 end
 
